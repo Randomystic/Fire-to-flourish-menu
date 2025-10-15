79c4ce8e08fdb7767c9c6bd121406866515666b6
aaro0024 <151136032+Randomystic@users.noreply.github.com>
2025-10-15 17:17:36 +1100

    Hooked up action card dropdowns to role titles and polished UX

--- My project/Assets/Scripts/Menu Scripts/PreperationPhase.cs
+++ My project/Assets/Scripts/Menu Scripts/PreperationPhase.cs
@@ -7,36 +7,65 @@ using TMPro;
 public class PreperationPhase : MonoBehaviour
 {
     public TMP_Dropdown dropdownPrefab;
-    public Transform dropdownParent;
+    public Transform roleTitlesParent;
     public Button saveButton;
+    public Vector2 dropdownSize = new Vector2(320, 34);
 
     public Dictionary<RoleType, string> selectedCardsDict = new();
+
+    [SerializeField] bool constantFontSize = true;
+    [SerializeField] float dropdownFontSize = 16f;
+
+    [SerializeField] Vector3 dropdownScale = new Vector3(0.25f, 0.25f, 1f);
+    [SerializeField] float dropdownOffsetX = 0f;
+    [SerializeField] float dropdownOffsetY = -50f;
 
     readonly Dictionary<RoleType, TMP_Dropdown> roleDropdowns = new();
     readonly Dictionary<RoleType, List<string>> roleCardIds = new();
 
     void Start()
     {
+        if (!roleTitlesParent)
+        {
+            var go = GameObject.Find("Canvas/RoleTitles");
+            if (go) roleTitlesParent = go.transform;
+        }
+
         foreach (RoleType role in Enum.GetValues(typeof(RoleType)))
         {
-            var dd = Instantiate(dropdownPrefab, dropdownParent);
-            dd.gameObject.name = role.ToString();
-            dd.ClearOptions();
+            var title = roleTitlesParent ? roleTitlesParent.Find(role.ToString()) : null;
+            if (!title)
+            {
+                var path = $"Canvas/RoleTitles/{role}";
+                var f = GameObject.Find(path);
+                if (!f) continue;
+                title = f.transform;
+            }
+
+            var roleDropdown = Instantiate(dropdownPrefab, title, false);
+            roleDropdown.gameObject.SetActive(true);
+            roleDropdown.gameObject.name = $"{role}_Dropdown";
+            roleDropdown.ClearOptions();
 
             if (RoleCards.roleCardsDict.TryGetValue(role, out var ids))
             {
                 roleCardIds[role] = ids;
                 var labels = new List<string>(ids.Count);
                 foreach (var id in ids) labels.Add(id.Replace("_", " "));
-                dd.AddOptions(labels);
+                roleDropdown.AddOptions(labels);
             }
-            else
-            {
-                roleCardIds[role] = new List<string>();
-            }
+            else roleCardIds[role] = new List<string>();
 
-            ConfigureTMP(dd);
-            roleDropdowns[role] = dd;
+            ConfigureTMP(roleDropdown);
+            var rectTransform = roleDropdown.GetComponent<RectTransform>();
+
+            rectTransform.anchorMin = new Vector2(0.5f, 1f);
+            rectTransform.anchorMax = new Vector2(0.5f, 1f);
+            rectTransform.pivot     = new Vector2(0.5f, 1f);
+            rectTransform.anchoredPosition = new Vector2(dropdownOffsetX, dropdownOffsetY);
+            rectTransform.localScale = dropdownScale;
+
+            roleDropdowns[role] = roleDropdown;
         }
 
         if (saveButton) saveButton.onClick.AddListener(Save);
@@ -48,28 +77,24 @@ public class PreperationPhase : MonoBehaviour
         foreach (var kv in roleDropdowns)
         {
             var role = kv.Key;
-            var dd = kv.Value;
-            if (dd.options.Count == 0) continue;
-
-            int idx = dd.value;
-            string pickedId = roleCardIds[role][idx];
-            selectedCardsDict[role] = pickedId;
+            var roleDropdown = kv.Value;
+            if (roleDropdown && roleDropdown.options.Count > 0)
+                selectedCardsDict[role] = roleCardIds[role][roleDropdown.value];
         }
     }
 
-    static void ConfigureTMP(TMP_Dropdown dd)
+    void ConfigureTMP(TMP_Dropdown roleDropdown)
     {
-        void Cfg(TMP_Text t)
+        void ConfigureDropdownLabel(TMP_Text txt)
         {
-            if (!t) return;
-            t.enableAutoSizing = true;
-            t.fontSizeMin = 8;
-            t.fontSizeMax = 28;
-            t.enableWordWrapping = false;
-            t.overflowMode = TextOverflowModes.Overflow;
-        }
+            if (!txt) return;
+            txt.enableAutoSizing = !constantFontSize;
 
-        Cfg(dd.captionText);
-        Cfg(dd.itemText);
+            if (constantFontSize) txt.fontSize = dropdownFontSize;
+            txt.enableWordWrapping = false;
+            txt.overflowMode = TextOverflowModes.Masking;
+        }
+        ConfigureDropdownLabel(roleDropdown.captionText);
+        ConfigureDropdownLabel(roleDropdown.itemText);
     }
 }
