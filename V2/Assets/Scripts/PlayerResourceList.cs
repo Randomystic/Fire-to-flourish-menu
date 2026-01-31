cbda4dc56027b14017232a08dd41c0a73f63368b
Randomystic <shooter5642@gmail.com>
2026-01-31 15:29:29 +1100

    Added Game Phases and ReflectionPhase script (commented out)

--- V2/Assets/Scripts/PlayerResourceList.cs
+++ V2/Assets/Scripts/PlayerResourceList.cs
@@ -5,27 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerResourceList
 {
-    public float money = 0;
-    public int morale = 0;
-    public int respect = 0;
-
-    public void AdjustMorale(int amount)
-    {
-        morale = Mathf.Clamp(morale + amount, 0, 25);
-    }
-
-    public void AdjustRespect(int amount)
-    {
-        respect = Mathf.Clamp(respect + amount, 0, 20);
-    }
+    public float money = 0; 
 
     public void AdjustMoney(int amount)
     {
         money = Mathf.Max(money + amount, 0);
     }
 
-    public void UpdateAllResources()
-    {
-        // Implement global updates here
-    }
 }
--- V2/Assets/Scripts/TurnManagement.cs
+++ V2/Assets/Scripts/TurnManagement.cs
@@ -9,13 +9,20 @@ public class TurnManagement : MonoBehaviour
 
     public Button endTurnButton;
     public TextMeshProUGUI turnText;
+    public TextMeshProUGUI phaseText;
+
 
     public int currentTurn = 1;
+    public int currentPhase = 1; // 1: Preparation, 2: Firebush, 3: Reflection
     // Start is called before the first frame update
     void Start()
     {
         currentTurn = 1;
         turnText.text = "Current Turn: " + currentTurn.ToString();
+
+        currentPhase = 1;
+        phaseText.text = "Current Phase: " + "Preparation";
+        
     }
 
     // Update is called once per frame
@@ -29,6 +36,19 @@ public class TurnManagement : MonoBehaviour
         currentTurn++;
         turnText.text = "Current Turn: " + currentTurn.ToString();
 
+        int phaseIndex = ((currentTurn - 1)/ 3) % 2; 
+
+        if (phaseIndex == 0)
+        {
+            currentPhase = 1;
+            phaseText.text = "Current Phase: Preparation";
+        }
+        else if (phaseIndex == 1)
+        {
+            currentPhase = 2;
+            phaseText.text = "Current Phase: Firebush";
+        }
+        
 
     }
 }
